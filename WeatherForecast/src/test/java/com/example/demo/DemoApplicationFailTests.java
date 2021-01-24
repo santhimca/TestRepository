b4c0878ec33206d20eca020dbd06113a20d73143
Shan Ch <santhi.mca@gmail.com>
2021-01-23 17:29:34 -0800

    Junit test cases added.

--- WeatherForecast/src/test/java/com/example/demo/DemoApplicationFailTests.java
+++ WeatherForecast/src/test/java/com/example/demo/DemoApplicationFailTests.java
@@ -6,11 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(args = "33.569592,-117.726372")
-class DemoApplicationTests {
+@SpringBootTest
+class DemoApplicationFailTests {
 
 	
-	  @Test void contextLoads() {	  
-	  }	 
+	  @Test void contextLoads() {}	 
 
 }
